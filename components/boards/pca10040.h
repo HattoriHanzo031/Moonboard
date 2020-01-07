87818ffb49963b7b250d5912a899845a354e9cc2
Filip Vranesevic <filip.vranesevic@seavus.com>
2020-01-07 12:49:31 +0100

    Set the borad configuration for breakout board

--- components/boards/pca10040.h
+++ components/boards/pca10040.h
@@ -47,44 +47,36 @@ extern "C" {
 #include "nrf_gpio.h"
 
 // LEDs definitions for PCA10040
-#define LEDS_NUMBER    4
+#define LEDS_NUMBER    2
 
 #define LED_START      17
 #define LED_1          17
 #define LED_2          18
-#define LED_3          19
-#define LED_4          20
-#define LED_STOP       20
+#define LED_STOP       18
 
 #define LEDS_ACTIVE_STATE 0
 
 #define LEDS_INV_MASK  LEDS_MASK
 
-#define LEDS_LIST { LED_1, LED_2, LED_3, LED_4 }
+#define LEDS_LIST { LED_1, LED_2 }
 
 #define BSP_LED_0      LED_1
 #define BSP_LED_1      LED_2
-#define BSP_LED_2      LED_3
-#define BSP_LED_3      LED_4
 
-#define BUTTONS_NUMBER 4
+#define BUTTONS_NUMBER 2
 
 #define BUTTON_START   13
 #define BUTTON_1       13
 #define BUTTON_2       14
-#define BUTTON_3       15
-#define BUTTON_4       16
-#define BUTTON_STOP    16
+#define BUTTON_STOP    14
 #define BUTTON_PULL    NRF_GPIO_PIN_PULLUP
 
 #define BUTTONS_ACTIVE_STATE 0
 
-#define BUTTONS_LIST { BUTTON_1, BUTTON_2, BUTTON_3, BUTTON_4 }
+#define BUTTONS_LIST { BUTTON_1, BUTTON_2}
 
 #define BSP_BUTTON_0   BUTTON_1
 #define BSP_BUTTON_1   BUTTON_2
-#define BSP_BUTTON_2   BUTTON_3
-#define BSP_BUTTON_3   BUTTON_4
 
 #define RX_PIN_NUMBER  8
 #define TX_PIN_NUMBER  6
